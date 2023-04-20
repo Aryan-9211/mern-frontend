7b945663119f79cde64df12d5cf79bd0d714ac6b
Aryan-9211 <manglaaryan178@gmail.com>
2023-04-20 16:58:31 +0530

    added auth in about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const About = () => {
+  const history = useHistory();
+  const [data, setData] = useState("");
+
+  const callAboutPage = async () => {
+    try {
+      const res = await fetch("/about", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+
+      const responseData = await res.json();
+
+      if (!res.ok) {
+        throw new Error(responseData.message);
+      } else {
+        setData(responseData);
+      }
+    } catch (err) {
+      console.log(err);
+      history.push("/signin");
+    }
+  };
+
+  useEffect(() => {
+    callAboutPage();
+  }, []);
+
   return (
     <div style={{ textAlign: "center", marginTop: "15%" }}>
       <p>WELCOME ABOUT</p>
-      <h1>We Are The MERN Developer</h1>
+      <h1>NAME: {data.name}</h1>
+      <h1>EMAIL: {data.email}</h1>
+      <h1>PHONE: {data.phone}</h1>
     </div>
   );
 };
